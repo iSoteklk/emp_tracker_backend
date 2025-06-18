74c83ce0e97d42f563f05c93c90745b2df93bea6
kaweendras <salitha.wijerathna@gmail.com>
2025-06-18 14:03:41 +0530

    CHG | add configuration file to manage environment variables

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  server: {
+    port: process.env.PORT || 4000,
+  },
+
+  database: {
+    mongoUri: process.env.MONGODB_URI as string,
+  },
+
+  auth: {
+    jwtSecret: process.env.JWT_SECRET as string,
+  },
+};
+
+export default config;
